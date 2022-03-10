c0c79cbfb7ebcbe22593eac753fd1a7e4dfdd3df
Ashik Rahman <ashrhmn99@gmail.com>
2022-03-11 03:56:36 +0600

    Add listing aircrafts and adding new

--- Controllers/FlightManagerController.cs
+++ Controllers/FlightManagerController.cs
@@ -1,4 +1,8 @@
 ﻿using Flight_Management_System.Auth;
+using Flight_Management_System.Models.AuthEntities;
+using Flight_Management_System.Models.Database;
+using Flight_Management_System.Models.FlightManagerEntities;
+using Flight_Management_System.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +13,58 @@ namespace Flight_Management_System.Controllers
 {
     public class FlightManagerController : Controller
     {
+        private Flight_ManagementEntities db;
+        private JwtManage jwt;
+        public FlightManagerController()
+        {
+            db = new Flight_ManagementEntities();
+            jwt = new JwtManage();
+        }
         [FlightManagerAccess]
+        [HttpGet]
         public ActionResult Dashboard()
         {
-            return View();
+            AuthPayload user = jwt.LoggedInUser(Request.Cookies);
+            var dbAirlines = db.Transports.Where(t => t.CreatedBy == user.Id).ToList();
+            List<AirlineModel> airlines = new List<AirlineModel>();
+            foreach (var airline in dbAirlines)
+            {
+                AirlineModel airlineModel = new AirlineModel()
+                {
+                    Id = airline.Id,
+                    Name = airline.Name,
+                    FromStopageId = airline.FromStopageId,
+                    FromAirport = airline.Stopage == null ? "Undefined" : airline.Stopage.Name,
+                    ToAirport = airline.Stopage1 == null ? "Undefined" : airline.Stopage1.Name,
+                    FromCity = airline.Stopage==null?"Undefined": airline.Stopage.City.Name,
+                    FromCountry = airline.Stopage == null ? "Undefined" : airline.Stopage.City.Country,
+                    ToStopageId =  airline.ToStopageId,
+                    ToCity = airline.Stopage1 == null ? "Undefined" : airline.Stopage1.City.Name,
+                    ToCountry = airline.Stopage1 == null ? "Undefined" : airline.Stopage1.City.Country,
+                    SeatCapacity = airline.MaximumSeat ?? 0
+                };
+                airlines.Add(airlineModel);
+            }
+            return View(airlines);
+        }
+
+        [FlightManagerAccess]
+        [HttpPost]
+        public ActionResult AddAircraft(string Name, int FromStopage, int ToStopage, int SeatCapacity)
+        {
+            AuthPayload user = jwt.LoggedInUser(Request.Cookies);
+            if (user == null) return RedirectToAction("SignIn", "Auth");
+            Transport transport = new Transport()
+            {
+                Name = Name,
+                FromStopageId = FromStopage,
+                ToStopageId= ToStopage,
+                MaximumSeat = SeatCapacity,
+                CreatedBy = user.Id
+            };
+            db.Transports.Add(transport);
+            db.SaveChanges();
+            return RedirectToAction("Dashboard");
         }
     }
 }
---
+++ Models/FlightManagerEntities/AirlineModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Flight_Management_System.Models.FlightManagerEntities
+{
+    public class AirlineModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? FromStopageId { get; set; }
+        public int? ToStopageId { get; set; }
+        public string FromAirport { get; set; }
+        public string ToAirport { get; set; }
+        public string FromCity { get; set; }
+        public string ToCity { get; set; }
+        public string FromCountry { get; set; }
+        public string ToCountry { get; set; }
+        public int SeatCapacity { get; set; }
+    }
+}
